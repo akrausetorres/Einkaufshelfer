885981d75c08130fa5740fa0db1b34454c7bd205
Bendix Kruse <>
2020-07-06 15:47:17 +0200

    initial start of project

---
+++ src/main/java/de/hsba/bi/einkaufshelfer/EinkaufshelferApplication.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.einkaufshelfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinkaufshelferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinkaufshelferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsba/bi/einkaufshelfer/EinkaufshelferApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsba.bi.einkaufshelfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinkaufshelferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
