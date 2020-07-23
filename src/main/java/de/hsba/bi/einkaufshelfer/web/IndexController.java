aa7698a66b00b4be9b2b6e89745821dfff435731
Bendix Kruse <>
2020-07-23 16:24:51 +0200

    add user list to homepage

--- src/main/java/de/hsba/bi/einkaufshelfer/web/IndexController.java
+++ src/main/java/de/hsba/bi/einkaufshelfer/web/IndexController.java
@@ -2,6 +2,7 @@ package de.hsba.bi.einkaufshelfer.web;
 
 import de.hsba.bi.einkaufshelfer.user.User;
 import de.hsba.bi.einkaufshelfer.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,17 +10,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
+
+    private  final UserService userService;
 
     @GetMapping("/")
     public String index(Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         String greetingString = auth instanceof AnonymousAuthenticationToken ? "!" : " " + SecurityContextHolder.getContext().getAuthentication().getName() + "!";
-
         model.addAttribute("pageTitle", "Hey" + greetingString);
         model.addAttribute("pageDescription", "Hier ensteht dein neuer Einkaufshelfer!");
+
+        List<User> helpers = userService.findHelpers();
+        List<User> needers = userService.findNeeders();
+        model.addAttribute("helpers", helpers);
+        model.addAttribute("needers", needers);
+
         return "homepage";
     }
 }
